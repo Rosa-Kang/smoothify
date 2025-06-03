3f4462491243a816abac8b44652929f6dea4fbad
Rosa Kang <rosakang@Rosas-MacBook-Air.local>
2025-06-03 10:04:44 -0400

    theme setting

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('content') as HTMLElement
@@ -11,7 +13,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
       <BrowserRouter>
-        <App />
+      <ThemeProvider theme={theme}>
+          <CssBaseline  />
+          <App />
+      </ThemeProvider>
       </BrowserRouter>
   </React.StrictMode>
 );
