8099130344a7ff795bbffe9beb184d3b920207a9
Rosa Kang <therosessom@gmail.com>
2025-06-17 11:24:57 -0400

    delete cache

--- src/pages/SearchWithKeyword/SearchWithKeyword.tsx
+++ src/pages/SearchWithKeyword/SearchWithKeyword.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router";
+import { SEARCH_TYPE } from "../../models/search"; 
 import { useSearchItemsByKeyword } from "../../hooks/uesSearchItemsByKeyword";
-import { SEARCH_TYPE } from "../../models/search";
 
 const SearchWithKeyword = () => {
   const { keyword = '' } = useParams<{ keyword: string }>();
